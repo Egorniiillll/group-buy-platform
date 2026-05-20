a552b4131589b445cf327ee03e6203e69767efb8
Egorniiillll <119012231+Egorniiillll@users.noreply.github.com>
2026-05-20 13:01:13 +0300

    structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { LoginPage } from './core/auth/login-page/login-page';
+import { OrdersPage } from './features/orders/pages/orders-page/orders-page';
+import { CreateOrderPage } from './features/orders/pages/create-order-page/create-order-page';
+import { OrderDetailsPage } from './features/orders/pages/order-details-page/order-details-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginPage,
+  },
+  {
+    path: 'orders',
+    component: OrdersPage,
+  },
+  {
+    path: 'orders/create',
+    component: CreateOrderPage,
+  },
+  {
+    path: 'orders/:id',
+    component: OrderDetailsPage,
+  },
+  {
+    path: '',
+    redirectTo: 'orders',
+    pathMatch: 'full',
+  },
+];
